62816443f04e55aebdf4528e716d6c0fa288e554
username <e-mail>
2023-03-27 09:05:12 +0200

    Vagrant server

--- LB2/script/script.sh
+++ LB2/script/script.sh
@@ -1,5 +1,13 @@
-sudo ufw enable
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install -y apache2
+sudo apt-get install -y ufw
+sudo apt-get install -y libapache2-mod-proxy-html
+sudo apt-get install -y libxml2-dev
+
+sudo ufw --force enable
 sudo ufw allow 80/tcp
+sudo ufw allow 22
 sudo ufw allow from [192.168.33.10] to any port 22
 
 sudo a2enmod proxy
@@ -7,3 +15,4 @@ sudo a2enmod proxy_html
 sudo a2enmod proxy_http
 
 nano /etc/apache2/apache2.conf  --ServerName localhost
+
